27a0c3f8193d6b071e712f914dbe17ed40d746f8
Lorenzo Smedile <Lorenzo Smedile>
2024-03-12 11:43:04 +0100

    secondo commit

--- RLE.java
+++ RLE.java
@@ -1,5 +1,7 @@
 package it.edu.iisgubbio.geometria;
 
+import java.util.Hashtable;
+
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
@@ -8,57 +10,61 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Button;
+import java.util.*;
 
-public class RLE extends Application{
+public class RLE extends Application {
+
+	TextField tPeso = new TextField();
+	Button bComprimi = new Button("COMPRIMI");
+	Label lPesoImmagineCompresso = new Label("PESO IMMAGINE COMPRESSO");
+	Label lCompressionRate = new Label("COMPRESSION RATE");
 	
-	TextField tPeso= new TextField();
-	Button bComprimi=new Button("COMPRIMI");
-	Label lPesoImmagineCompresso= new Label("PESO IMMAGINE COMPRESSO");
-	Label lCompressionRate= new Label("COMPRESSION RATE");
-	
-	
+	String matrice[][];
+
 	public void start(Stage finestra) {
-	
-	
+
 		GridPane griglia = new GridPane();
-	
-	
-	griglia.add(tPeso, 0, 0);
-	griglia.add(bComprimi, 0, 1);
-	griglia.add(lPesoImmagineCompresso, 0, 2);
-	griglia.add(lCompressionRate, 0, 3);
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
+		griglia.add(tPeso, 0, 0);
+		griglia.add(bComprimi, 0, 1);
+		griglia.add(lPesoImmagineCompresso, 0, 2);
+		griglia.add(lCompressionRate, 0, 3);
+
 		Scene scena = new Scene(griglia);
+		
 
 		finestra.setTitle("Convertitore");
 		finestra.setScene(scena);
 		finestra.show();
-	
-	
-	
-	
-	
-	
+
+		bComprimi.setOnAction(e -> sopprimi());
+
 	}
-	
+
+	public void sopprimi() {
+		
+
+		int riga=3;
+		int colonna=3;
+		matrice=new String [riga][colonna];
+		
+		for (int i=0;i<riga;i++) {
+			Hashtable<String, Integer> my_dict= new Hashtable<String,Integer>();
+			for (int i2=0;i2<colonna;i2++) {
+				if (my_dict.containsKey(matrice[i][i2])) {
+					
+					int val = my_dict.get(matrice[i][i2]);
+					my_dict.get(val+1);
+				} else {
+					my_dict.put(matrice[i][i2],1);
+
+					
+				}
+			}
+		}
+
+	}
+
 	public static void main(String[] args) {
 		launch(args);
 	}
